b08462eaf860eace0626b864fec59f1d261b284b
Achmad Ilham <30348815+achmadilham07@users.noreply.github.com>
2021-05-12 11:07:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,63 @@
 const http = require('http');
- 
+
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
- 
-    response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>');
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('X-Powered-By', 'NodeJS');
+
+    const { method, url } = request;
+
+    if (url === '/') {
+        if (method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: 'Ini adalah homepage',
+            }));
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message: 'Halaman tidak ditemukan!',
+            }));
+        }
+    } else if (url === '/about') {
+        if (method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: 'Halo! Ini adalah halaman about',
+            }));
+        } else if (method === 'POST') {
+            let body = [];
+
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.statusCode = 200;
+                response.end(JSON.stringify({
+                    message: `Halo, ${name}! Ini adalah halaman about`,
+                }));
+            });
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message: 'Halaman tidak ditemukan!',
+            }));
+        }
+    } else {
+        response.statusCode = 404;
+        response.end(JSON.stringify({
+            message: 'Halaman tidak ditemukan!',
+        }));
+    }
 };
- 
- 
+
 const server = http.createServer(requestListener);
- 
+
 const port = 5000;
 const host = 'localhost';
- 
+
 server.listen(port, host, () => {
     console.log(`Server berjalan pada http://${host}:${port}`);
 });
